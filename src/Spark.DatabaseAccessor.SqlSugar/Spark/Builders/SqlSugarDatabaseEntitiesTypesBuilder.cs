1177f13386187c8c820305b2b43b74449daa15a0
L.WLe <184483622@qq.com>
2022-10-13 15:18:55 +0800

    ✨ feat: 实体特性SqlTable配置构造器SqlSugarDatabaseEntitiesTypesBuilder

---
+++ src/Spark.DatabaseAccessor.SqlSugar/Spark/Builders/SqlSugarDatabaseEntitiesTypesBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Spark.DatabaseAccessor.Attributes;
+using SqlSugar;
+using System.Reflection;
+
+namespace Spark.DatabaseAccessor.SqlSugar.Builders
+{
+    /// <summary>
+    ///     实体特性SqlTable配置构造器
+    /// </summary>
+    internal class SqlSugarDatabaseEntitiesTypesBuilder
+    {
+        /// <summary>
+        ///     构造
+        /// </summary>
+        /// <param name="type">实体类型</param>
+        /// <param name="entity">实体属性</param>
+        internal static void Build(Type type, EntityInfo entity)
+        {
+            var sqltable = type.GetCustomAttribute<SqlTableAttribute>();
+            if (sqltable == null) return;
+            entity.DbTableName = sqltable.TableName.IsNullOrEmpty() ? type.Name : sqltable.TableName;
+            entity.TableDescription = sqltable.TableDescription;
+            entity.IsDisabledUpdateAll = sqltable.IsDisabledUpdateAll;
+            entity.IsDisabledDelete = sqltable.IsDisabledDelete;
+        }
+    }
+}
---
+++ src/Spark.DatabaseAccessor/Attributes/SqlTableAttribute.cs
@@ -0,0 +1,62 @@
+﻿namespace Spark.DatabaseAccessor.Attributes
+{
+    /// <summary>
+    ///     数据实体特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class SqlTableAttribute : Attribute
+    {
+        /// <summary>
+        ///     表名
+        /// </summary>
+        public string? TableName { get; set; }
+
+        /// <summary>
+        ///     表描述
+        /// </summary>
+        public string? TableDescription { get; set; }
+
+        /// <summary>
+        ///   数据库连接标识
+        /// <remarks>
+        ///   如果不指定，则使用默认连接
+        /// </remarks>
+        /// </summary>
+        public string? ConnectionName { get; set; }
+
+        /// <summary>
+        ///     禁止删除列
+        /// </summary>
+        public bool IsDisabledDelete { get; set; }
+
+        /// <summary>
+        ///     禁止更新数据库
+        /// </summary>
+        public bool IsDisabledUpdateAll { get; set; }
+
+        /// <summary>
+        ///     构造函数
+        /// </summary>
+        public SqlTableAttribute() { }
+
+        /// <summary>
+        ///     构造函数
+        /// </summary>
+        /// <param name="tableName">表名</param>
+        public SqlTableAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        /// <summary>
+        ///     构造函数
+        /// </summary>
+        /// <param name="tableName">表名</param>
+        /// <param name="connectionName">数据库连接标识</param>
+        public SqlTableAttribute(string tableName, string connectionName)
+        {
+            TableName = tableName;
+            ConnectionName = connectionName;
+        }
+    }
+}
